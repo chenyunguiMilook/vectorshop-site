3c10a8fb670386bf3217d3e8e955827ab3df84c7
chenyunguiMilook <linxitj2003@icloud.com>
2024-11-05 20:28:23 +0800

    change config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,16 +10,17 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/vectorshop-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'chenyunguiMilook', // Usually your GitHub org/user name.
+  projectName: 'vectorshop-site', // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
